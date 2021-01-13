cb1a0f4257b1a3e1caa00ed9963ee1685ae5145b
Zoltán Király <zoliky@gmail.com>
2021-01-13 17:38:20 +0200

    Sync

--- themes/basic/assets/js/main.js
+++ themes/basic/assets/js/main.js
@@ -3,3 +3,19 @@
 // Check if Babel is enabled
 const isBabel = !(class {}.toString().indexOf('class ') === 0);
 console.log("Babel enabled:", isBabel);
+
+// Menu
+const menuToggleBtn = document.querySelector(".menu-toggle");
+menuToggleBtn.addEventListener("click", event => {
+  event.preventDefault();
+  menuToggleBtn.parentElement.classList.toggle("toggled-on");
+});
+
+const dropdownToggleBtn = document.querySelectorAll(".dropdown-toggle");
+dropdownToggleBtn.forEach(item => {
+  item.addEventListener("click", event => {
+    event.preventDefault();
+    item.classList.toggle("toggled-on");
+    item.nextElementSibling.classList.toggle("toggled-on");
+  });
+});
